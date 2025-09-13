a23d1879b0e3e88d612c6298c949390480adcbdf
paplauskis <paplauskaskostas@gmail.com>
2025-09-13 21:43:00 +0300

    commit

---
+++ Api/Startup.cs
@@ -0,0 +1,19 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
