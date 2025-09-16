6b1b1d49879f5cdbe87029d065225817bedb3555
paplauskis <paplauskaskostas@gmail.com>
2025-09-16 19:30:17 +0300

    create DbSets, apply model configurations

---
+++ Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+using Domain.Models;
+using Infrastructure.Context.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) { }
+ 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<Address>  Addresses { get; set; } = null!;
+    public DbSet<User> Users { get; set; }  = null!;
+    public DbSet<ItemType> ItemTypes { get; set; }  = null!;
+    public DbSet<FoundItemStatus>  FoundItemStatuses { get; set; }  = null!;
+    public DbSet<LostItem>  LostItems { get; set; }  = null!;
+    public DbSet<FoundItem>  FoundItems { get; set; } = null!;
+}
