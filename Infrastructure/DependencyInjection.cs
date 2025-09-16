283e15f64426a30da7e4cb98045dae8e0f324b2b
paplauskis <paplauskaskostas@gmail.com>
2025-09-16 19:22:32 +0300

    add infrastructure DI extension to register AppDbContext

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    // add DbContext so installing EF is not needed in Api project
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+        
+        return services;
+    }
+}
