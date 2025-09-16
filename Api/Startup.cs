12c7ea169b9fb2d645eb0b23f0b5b54979131f47
paplauskis <paplauskaskostas@gmail.com>
2025-09-16 19:40:09 +0300

    add db context

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,10 +1,14 @@
 using Serilog;
+using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
+
+var connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection");
+builder.Services.AddInfrastructure(connectionString!);
 
 var app = builder.Build();
 
