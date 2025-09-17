98212dbf9ef833ba33a3ae6943366edcc1dc6d45
paplauskis <paplauskaskostas@gmail.com>
2025-09-17 09:17:39 +0300

    add migration

---
+++ Infrastructure/Migrations/20250916192635_Initial.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "address",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    country = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    city = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    street = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    building_number = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    latitude = table.Column<double>(type: "double precision", nullable: false),
+                    longitude = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_address", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "found_item_status",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_found_item_status", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "item_type",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_item_type", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    username = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "item",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type_id = table.Column<int>(type: "integer", nullable: false),
+                    address_id = table.Column<int>(type: "integer", nullable: false),
+                    description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    ItemTypeId = table.Column<int>(type: "integer", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_item", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Item_Address",
+                        column: x => x.address_id,
+                        principalTable: "address",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Item_Type",
+                        column: x => x.type_id,
+                        principalTable: "item_type",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_item_item_type_ItemTypeId",
+                        column: x => x.ItemTypeId,
+                        principalTable: "item_type",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "found_item",
+                columns: table => new
+                {
+                    item_id = table.Column<int>(type: "integer", nullable: false),
+                    status_id = table.Column<int>(type: "integer", nullable: false),
+                    last_status_update_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    image_url = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_found_item", x => x.item_id);
+                    table.ForeignKey(
+                        name: "FK_FoundItem_Item",
+                        column: x => x.item_id,
+                        principalTable: "item",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FoundItem_Status",
+                        column: x => x.status_id,
+                        principalTable: "found_item_status",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "lost_item",
+                columns: table => new
+                {
+                    item_id = table.Column<int>(type: "integer", nullable: false),
+                    lost_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    reward_amount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_lost_item", x => x.item_id);
+                    table.ForeignKey(
+                        name: "FK_LostItem_Item",
+                        column: x => x.item_id,
+                        principalTable: "item",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_found_item_status_id",
+                table: "found_item",
+                column: "status_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_item_address_id",
+                table: "item",
+                column: "address_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_item_ItemTypeId",
+                table: "item",
+                column: "ItemTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_item_type_id",
+                table: "item",
+                column: "type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_email",
+                table: "user",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "found_item");
+
+            migrationBuilder.DropTable(
+                name: "lost_item");
+
+            migrationBuilder.DropTable(
+                name: "user");
+
+            migrationBuilder.DropTable(
+                name: "found_item_status");
+
+            migrationBuilder.DropTable(
+                name: "item");
+
+            migrationBuilder.DropTable(
+                name: "address");
+
+            migrationBuilder.DropTable(
+                name: "item_type");
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20250916193125_Initial3.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_item_item_type_ItemTypeId1",
+                table: "item");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemTypeId1",
+                table: "item",
+                newName: "ItemTypeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_item_ItemTypeId1",
+                table: "item",
+                newName: "IX_item_ItemTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_item_item_type_ItemTypeId",
+                table: "item",
+                column: "ItemTypeId",
+                principalTable: "item_type",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_item_item_type_ItemTypeId",
+                table: "item");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemTypeId",
+                table: "item",
+                newName: "ItemTypeId1");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_item_ItemTypeId",
+                table: "item",
+                newName: "IX_item_ItemTypeId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_item_item_type_ItemTypeId1",
+                table: "item",
+                column: "ItemTypeId1",
+                principalTable: "item_type",
+                principalColumn: "id");
+        }
+    }
+}
