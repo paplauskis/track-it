7dfbf6c27f694ce512cc6894ac8c1facf299085c
paplauskis <paplauskaskostas@gmail.com>
2025-09-17 09:04:33 +0300

    change class to non-abstract

--- Domain/Base/Item.cs
+++ Domain/Base/Item.cs
@@ -2,7 +2,7 @@ using Domain.Models;
 
 namespace Domain.Base;
 
-public abstract class Item : TimestampedEntity
+public class Item : TimestampedEntity
 {
     public ItemType Type { get; set; }
     
@@ -10,6 +10,7 @@ public abstract class Item : TimestampedEntity
     
     // if an item was lost, then address means last seen; otherwise address is for where an item was found
     public Address Address { get; set; }
+    
     public int AddressId { get; set; }
     
     public string Description { get; set; }
