6c04deb7872a9bb7561df83a7b9c28e8182fd506
paplauskis <paplauskaskostas@gmail.com>
2025-09-17 18:30:49 +0300

    create base repo class, implement base methods

---
+++ Infrastructure/Repositories/Generic/BaseRepository.cs
@@ -0,0 +1,53 @@
+using Domain.Base;
+using Domain.Interfaces.Repositories.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Generic;
+
+public abstract class BaseRepository<TEntity, TContext> :
+    IReadRepository<TEntity>,
+    IWriteRepository<TEntity>
+    where TEntity : BaseEntity
+    where TContext : DbContext
+{
+    protected readonly TContext Context;
+    protected DbSet<TEntity> Entities;
+
+    protected BaseRepository(TContext context)
+    {
+        Context = context;
+        Entities = Context.Set<TEntity>();
+    }
+    
+    public virtual async Task<TEntity?> GetByIdAsync(int id)
+    {
+        return await Entities.FindAsync(id);
+    }
+
+    public virtual async Task<IReadOnlyCollection<TEntity>> GetAllAsync()
+    {
+        return await Entities.AsNoTracking().ToListAsync();
+    }
+
+    public virtual async Task<TEntity> AddAsync(TEntity entity)
+    {
+        await Entities.AddAsync(entity);
+        await Context.SaveChangesAsync();
+        
+        return entity;
+    }
+
+    public virtual async Task<TEntity> UpdateAsync(TEntity updatedEntity)
+    {
+        Context.Update(updatedEntity);
+        await Context.SaveChangesAsync();
+        
+        return updatedEntity;
+    }
+
+    public virtual async Task DeleteAsync(TEntity entity)
+    {
+        Entities.Remove(entity);
+        await Context.SaveChangesAsync();
+    }
+}
