fe0b276c7af629692eb731d2f77d7c4e3f039232
paplauskis <paplauskaskostas@gmail.com>
2025-09-22 11:41:37 +0300

    create model

---
+++ Domain/Models/GeoAddress.cs
@@ -0,0 +1,8 @@
+namespace Domain.Models;
+
+public class GeoAddress
+{
+    public double Latitude { get; set; }
+    
+    public double Longitude { get; set; }
+}
---
+++ Domain/Models/PostalAddress.cs
@@ -0,0 +1,12 @@
+namespace Domain.Models;
+
+public class PostalAddress
+{
+    public string Country { get; set; }
+    
+    public string City { get; set; }
+    
+    public string Street { get; set; }
+    
+    public string BuildingNumber { get; set; }
+}
