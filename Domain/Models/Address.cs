822034be979579aa6a47d0bbee42b9c53f308ebf
paplauskis <paplauskaskostas@gmail.com>
2025-09-22 11:41:25 +0300

    move properties to separate owned models

--- Domain/Models/Address.cs
+++ Domain/Models/Address.cs
@@ -4,15 +4,7 @@ namespace Domain.Models;
 
 public class Address : BaseEntity
 {
-    public string Country { get; set; }
+    public PostalAddress Postal { get; set; }
     
-    public string City { get; set; }
-    
-    public string Street { get; set; }
-    
-    public string BuildingNumber { get; set; }
-    
-    public double Latitude { get; set; }
-    
-    public double Longitude { get; set; }
+    public GeoAddress Geo { get; set; }
 }
